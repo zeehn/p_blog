7337169c08b480f1b9500e15567e63df6711cb03
Zakir Hussain <zack.mesacc@gmail.com>
2020-03-14 15:14:05 +0500

    Added crud operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
 
     def new
         @article = Article.new
@@ -6,12 +9,35 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        if @article.save
+            redirect_to article_path(@article)
+        else
+            redirect_to new_article_path
+        end
     end
 
     def show
+        @article = Article.find(params[:id])
+    end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to article_path(@article)
+        else
+            redirect_to edit_article_path
+        end
+    end
+
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end
 
     private
