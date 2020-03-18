ca24ba6e754ebf10628518951da51d52d2f9c42f
Zakir Hussain <zack.mesacc@gmail.com>
2020-03-19 00:12:21 +0500

    Style index page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-    validates :description, presence: true, length: { minimum: 5, maximum: 100 }
+    validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 5, maximum: 10000 }
 end
