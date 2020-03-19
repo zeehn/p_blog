411b0c9324772750dc2cdda3b7d9e53adb0867ac
Zakir Hussain <zack.mesacc@gmail.com>
2020-03-19 23:34:07 +0500

    Add secure password and column to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 50 }
     VALID_EMAIL_REGEX = /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/i
     validates :email, presence: true, uniqueness: { case_sensitive: false}, length: { minimum: 5, maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200319154311) do
+ActiveRecord::Schema.define(version: 20200319182931) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20200319154311) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
