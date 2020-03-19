c516f5aac1c8867462eec53a0ba886cc688f2f24
Zakir Hussain <zack.mesacc@gmail.com>
2020-03-19 20:03:51 +0500

    add user validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ApplicationRecord
-    belongs_to :user
     validates :title, presence: true, length: { minimum: 3, maximum: 100 }
     validates :description, presence: true, length: { minimum: 5, maximum: 10000 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-    has_many :articles
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/i
+    validates :email, presence: true, uniqueness: { case_sensitive: false}, length: { minimum: 5, maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
 end
