5a0a6bfe3b01d42d0e2b8aac4cee2c98834800e2
Zakir Hussain <zack.mesacc@gmail.com>
2020-03-20 23:17:28 +0500

    bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show] 
-    before_action :require_user, except: [:show, :index]
     before_action :require_same_user, only: [:edit, :update]
 
     def index
