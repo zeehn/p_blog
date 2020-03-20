b72bb930066156c43e58f1f2a0dcd8643d2c8534
Zakir Hussain <zack.mesacc@gmail.com>
2020-03-20 23:13:34 +0500

    add user authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :show, :update, :destroy]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+
     def index
         @articles = Article.paginate(page: params[:page], per_page: 5)
     end
@@ -9,7 +12,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:success] = "Article created successfully."
             redirect_to article_path(@article)
@@ -19,15 +22,12 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:success] = "Article updated successfully."
             redirect_to article_path(@article)
@@ -38,7 +38,6 @@ class ArticlesController < ApplicationController
 
 
     def destroy
-        @article = Article.find(params[:id])
         if @article.destroy
             flash[:danger] = "Article deleted successfully."
             redirect_to articles_path
@@ -48,5 +47,16 @@ class ArticlesController < ApplicationController
     private
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def require_same_user
+        if current_user != @article.user
+            flash[:danger] = "You are not authorized to perform this operation."
+            redirect_to root_path
+        end
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show] 
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update]
 
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -19,11 +22,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "User updated successfully!"
             redirect_to articles_path
@@ -33,12 +34,22 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
     private
     def user_params
        params.require(:user).permit(:username, :email, :password) 
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def require_same_user
+        if @user != current_user
+            flash[:danger] = "You are not authorized to perform this operation."
+            redirect_to root_path
+        end
     end
 end
