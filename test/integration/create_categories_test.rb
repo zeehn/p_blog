ac244d0e44847951d8d87bf21cc06f4c8b2c1afb
Zakir Hussain <zack.mesacc@gmail.com>
2020-03-22 07:52:27 +0500

    Add categories to navbar

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+   def setup
+      @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+   end
+
     test "get new categoy form and create category" do 
+      sign_in_as(@user, "password")
        get new_category_path 
        assert_template 'categories/new'
        assert_difference 'Category.count', 1 do
@@ -13,11 +18,12 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
        assert_match "Sports", response.body
     end
 
-    test "invalid category submission results in failure" do 
+    test "invalid category submission results in failure" do
+        sign_in_as(@user, "password") 
         get new_category_path 
         assert_template 'categories/new'
         assert_no_difference 'Category.count' do
-         post categories_path, params: { category: { "name": " "}}
+         post categories_path, params: { category: { "name": ""}}
         end
         assert_template 'categories/new'
         assert_select 'h2.panel-title'
